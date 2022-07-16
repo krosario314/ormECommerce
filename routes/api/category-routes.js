46ecf64969903031935271bbae5d90f69000f581
Kayla Rosario <rosariok314@gmail.com>
2022-07-16 19:03:59 -0400

    find cate by id with products related, creating new cate, update id value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,20 +14,52 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// new categories
+router.post('/', async (req, res) => {
+  try {
+    const catData = await Category.create(req.body);
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.put('/:id', async (req, res) => {
+  try {
+    const catData = await Category.update(
+      {
+        id: req.body.id,
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
